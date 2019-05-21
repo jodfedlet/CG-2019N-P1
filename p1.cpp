6c0587a5c12b12a72b021184c397c8a3457bcc00
Jod Fedlet PIERRE <fedletpierre15@gmail.com>
2019-05-20 22:10:01 -0300

    Files

--- p1.cpp
+++ p1.cpp
@@ -11,9 +11,14 @@ to compile
 
 */
 
-GLfloat angleZ = 0.0f;
-GLfloat angleX = 0.0f;
+GLfloat translate = 5;
+GLfloat angleX = 0;
+GLfloat angleY = 0;
 
+//Define how many frames per seconds we want our
+// applications to run.
+const unsigned int FRAMES_PER_SECOND = 30;
+const unsigned int UPDATE_INTERVAL_MS = 1000 / FRAMES_PER_SECOND;
 
 void cube(){
 	glBegin(GL_QUADS);
@@ -66,12 +71,12 @@ void cube(){
 
 void Display(){
 	// Set Background Color
-	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 	// Clear screen
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// Reset transformations
-	//glLoadIdentity();
+	glLoadIdentity();
 
 	   glMatrixMode(GL_PROJECTION);
 	   glLoadIdentity( );
@@ -81,25 +86,70 @@ void Display(){
 	   glLoadIdentity( );
 	   gluLookAt(3, 4, 2,  //from. Posicao onde a camera esta posicionada
 	             0, 0, 0,  //to. Posicao absoluta onde a camera esta vendo
-	             0, 0, 1); //up. Vetor Up.
+	             0, 0, 0.5); //up. Vetor Up.
 
 	    
 	    
 
+	   	
+	   	glRotatef(angleY,0.0f,1.0f,0.0f);
 	    glPushMatrix();
-	    glRotatef(angleZ,0.0f,1.0f,0.0f);
-	    	 cube();//chamando o 1o cubo
+	    //glRotatef(angleX,2.0f,0.0f,0.0f);
+	   			cube();//chamando o 1o cubo
 	    glPopMatrix();
+
+	   
+	    glTranslatef(translate,0.0f,0.0f);
+	 
 
 	    glPushMatrix();
 	   	glRotatef(angleX,1.0f,0.0f,0.0f);
 	    	 cube();//chamando o 1o cubo
 	    glPopMatrix();
 
-		glFlush();
+		//glFlush();
 		glutSwapBuffers();
 
 	}
+
+	void update(int value)
+{
+	// Update the angle of rotation
+	angleY += 1;
+	
+	angleX += 3;
+
+	// Request a new frame rendering
+	glutPostRedisplay();
+
+	// Re-schedule the update() function to be called after a few
+	// milliseconds again.
+	glutTimerFunc(UPDATE_INTERVAL_MS, update, 0);
+}
+
+
+void mouse(int button, int state, int x, int y)
+{
+	std::cout << "Mouse pressed: button=" << button << ", state=" << state << ", x=" << x << " y=" << y << std::endl;
+}
+
+void initView()
+{
+	// We want a window to the world that is a rectangle starting at (0,0)
+	// and ending at (WIDTH, HEIGHT).
+	glViewport(0, 0, 640, 480);
+	
+	// Adjust the global projection (camera)
+	glMatrixMode(GL_PROJECTION);
+	
+	// Reset any existing projection settings and adjust the field-of-view (FOV)
+	glLoadIdentity();
+	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 200.0);
+	
+	// From now on, every transformation is to be applied on each object, e.g. modelview.
+	glMatrixMode(GL_MODELVIEW);
+}
+
 
 int main(int argc, char** argv){
 
@@ -118,6 +168,9 @@ int main(int argc, char** argv){
 	glEnable(GL_DEPTH_TEST);
 	
 	glutDisplayFunc(Display);
+	// Schedule the update() function to be called after a few
+	// milliseconds (calculated as 1000 milliseconds divided by FPS).
+	glutTimerFunc(UPDATE_INTERVAL_MS, update, 0);
 	
 	glutMainLoop();
 	
